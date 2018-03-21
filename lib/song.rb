ef9eaf76ac72251a3471eab9c88c657d22ada31e
Parul Gupta <parulguptasmail@gmail.com>
2018-03-21 18:38:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,40 @@ class Song
   def self.all
     @@all
   end
-
+  
+  def initialize(name = nil)
+    @name = name
+  end
+  
   def save
     self.class.all << self
   end
 
-end
+  def self.create
+    new_song = self.new
+    self.all << new_song
+    new_song
+  end
+  
+  def self.new_by_name(name)
+    new_song = self.new(name)
+  end
+  
+  def self.create_by_name(name)
+    new_song = new_by_name(name)
+    self.all << new_song
+    new_song
+  end
+  
+  def self.find_by_name(name)
+    self.all.detect { |song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    if song = self.find_by_name(name) == nil
+       self.create_by_name(name)
+     else
+       
+     end
+   end
+  end
